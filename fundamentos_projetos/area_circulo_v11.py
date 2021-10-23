d601e19b2e29416e9fd62e146947da84fd354778
ClaudiaThalita <claudiathalitamendes@outlook.com>
2021-10-23 18:21:20 -0300

    feature: finalizaçao do modulo 2 pratico do curso de python

---
+++ fundamentos_projetos/area_circulo_v11.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+from math import pi
+import sys
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+if __name__ == '__main__':
+    if len(sys.argv) <2:
+        print(f"""\
+            É necessário informar o raio do círculo.
+            Sintaxe: {sys.argv[0]} <raio> """)
+    else:
+        raio = sys.argv[1]
+        area = circulo(raio)
+        print('Àrea do círculo', area)
---
+++ fundamentos_projetos/area_circulo_v15.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+from math import pi
+import sys
+import errno
+
+ERRO = '\033[91m'
+NORMAL = '\033[0m'
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+def help():
+    print(f"""\
+        É necessário informar o raio do círculo.
+        Sintaxe: {sys.argv[0]} <raio> """)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <2:
+        help()
+        sys.exit(errno.EPERM)
+
+
+    if not sys.argv[1].isnumeric():
+        help()
+        print(ERRO + """\
+        O raio deve ser um valor númerico""" + NORMAL)
+        sys.exit(errno.EINVAL)
+
+    raio = sys.argv[1]
+    area = circulo(raio)
+    print('Àrea do círculo', area)
